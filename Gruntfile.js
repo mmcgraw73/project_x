97b2a8571837c2ac2e19b7006678ae38d93f1d80
mmcgraw73 <michael@mcgraw.codes>
2017-02-14 14:54:52 -0500

    updated gruntfile and added main.scss for compiliation of partials

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,84 +1,87 @@
 module.exports = function(grunt) {
 
-  // ===========================================================================
-  // CONFIGURE GRUNT ===========================================================
-  // ===========================================================================
-  grunt.initConfig({
+    // ===========================================================================
+    // CONFIGURE GRUNT ===========================================================
+    // ===========================================================================
+    grunt.initConfig({
 
-    // get the configuration info from package.json ----------------------------
-    // this way we can use things like name and version (pkg.name)
-    pkg: grunt.file.readJSON('package.json'),
+        // get the configuration info from package.json ----------------------------
+        // this way we can use things like name and version (pkg.name)
+        pkg: grunt.file.readJSON('package.json'),
 
-    // configure jshint to validate js files -----------------------------------
-    jshint: {
-      options: {
-        reporter: require('jshint-stylish'),
-        force: true,
-        reporterOutput: ""
-      },
-      build: ['Grunfile.js', 'src/**/*.js']
-      //all: ['Grunfile.js', 'src/js/*.js']
-    },
+        // configure jshint to validate js files -----------------------------------
+        jshint: {
+            options: {
+                reporter: require('jshint-stylish'),
+                force: true,
+                reporterOutput: ""
+            },
+            build: ['Grunfile.js', 'src/**/*.js']
+            //all: ['Grunfile.js', 'src/js/*.js']
+        },
 
-    // configure uglify to minify js files -------------------------------------
-    uglify: {
-      options: {
-        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n <%= pkg.author %>\n*/\n'
-      },
-      build: {
-        files: {
-          'dist/js/<%= pkg.name %>.min.js': 'src/**/*.js'
+        // configure uglify to minify js files -------------------------------------
+        uglify: {
+            options: {
+                banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n <%= pkg.author %>\n*/\n'
+            },
+            build: {
+                files: {
+                    'dist/js/<%= pkg.name %>.min.js': 'src/**/*.js'
+                }
+            }
+        },
+
+        // compile scss stylesheets to css -----------------------------------------
+        sass: {
+            options: {
+                style: 'expanded'
+            },
+            dist: {
+                files: {
+                    'dist/css/main.css': 'src/scss/main.scss'
+                }
+            }
+        },
+
+        // configure cssmin to minify css files ------------------------------------
+        cssmin: {
+            options: {
+                banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n <%= pkg.author %>\n*/\n'
+            },
+            build: {
+                files: {
+                    'wwwroot/css/<%= pkg.name %>.min.css': 'src/**/*.css'
+                }
+            }
+        },
+
+        // configure watch to auto update ------------------------------------------
+        watch: {
+            stylesheets: {
+                files: ['src/css/<%= pkg.name %>.css', 'src/scss/*.scss'],
+                tasks: ['sass', 'cssmin']
+            },
+            scripts: {
+                files: 'src/js/*.js',
+                tasks: ['jshint', 'uglify']
+            }
         }
-      }
-    },
 
-    // compile scss stylesheets to css -----------------------------------------
-    sass: {
-      build: {
-        files: {
-          'dist/css/<%= pkg.name %>.css': 'src/scss/*.scss'
-        }
-      }
-    },
+    });
 
-    // configure cssmin to minify css files ------------------------------------
-    cssmin: {
-      options: {
-        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
-      },
-      build: {
-        files: {
-          'dist/css/<%= pkg.name %>.min.css': 'src/scss/<%= pkg.name %>.css'
-        }
-      }
-    },
+    // ===========================================================================
+    // LOAD GRUNT PLUGINS ========================================================
+    // ===========================================================================
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    // configure watch to auto update ------------------------------------------
-    watch: {
-      stylesheets: {
-        files: ['src/css/*.css', 'src/scss/*.scss'],
-        tasks: ['sass', 'cssmin']
-      },
-      scripts: {
-        files: 'src/js/*.js',
-        tasks: ['jshint', 'uglify']
-      }
-    }
-
-  });
-
-  // ===========================================================================
-  // LOAD GRUNT PLUGINS ========================================================
-  // ===========================================================================
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-
-  // ===========================================================================
-  // CREATE TASKS ==============================================================
-  // ===========================================================================
-  grunt.registerTask('default', ['jshint', 'uglify', 'cssmin', 'sass']);
+    // ===========================================================================
+    // CREATE TASKS ==============================================================
+    // ===========================================================================
+    grunt.registerTask('default', ['jshint', 'uglify', 'sass', 'cssmin']);
 
 };
