55bf24c8d38c437f772e7cde9b8667039cdea109
mmcgraw73 <michael@mcgraw.codes>
2017-02-19 02:51:18 -0500

    test app complete

--- wwwroot/src/js/main.js
+++ wwwroot/src/js/main.js
@@ -1,3 +1,17 @@
-var turd = 'stinky';
-var stinky = 'turd';
-console.log('------- main.js compiled + loaded!! ------------');
+$(document).ready(function() {
+    console.log('turrrrds');
+    addElement();
+});
+
+
+function addElement () {
+  // create a new div element
+  // and give it some content
+  var newDiv = document.getElementById("dataHold");
+  var newContent = document.createTextNode("Hi there and greetings!");
+  newDiv.appendChild(newContent); //add the text node to the newly created div.
+
+  // add the newly created element and its content into the DOM
+  var currentDiv = document.getElementById("div1");
+  document.body.insertBefore(newDiv, currentDiv);
+}
