f80802e9e6584a7dc2db03549e6e0f30b51dfc8e
mmcgraw73 <michael@mcgraw.codes>
2017-02-19 10:10:05 -0500

    removed testing comments

--- wwwroot/src/js/main.js
+++ wwwroot/src/js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    console.log('turrrrds');
     addElement();
 });
 
@@ -10,7 +9,6 @@ function addElement () {
   var newDiv = document.getElementById("dataHold");
   var newContent = document.createTextNode("Hi there and greetings!");
   newDiv.appendChild(newContent); //add the text node to the newly created div.
-
   // add the newly created element and its content into the DOM
   var currentDiv = document.getElementById("div1");
   document.body.insertBefore(newDiv, currentDiv);
