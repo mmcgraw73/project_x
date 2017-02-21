c35966a7e90be0ff7949f4a4f74f4b495e49b9b8
mmcgraw73 <michael@mcgraw.codes>
2017-02-21 13:24:01 -0500

    cleared main.js unused function

--- wwwroot/src/js/main.js
+++ wwwroot/src/js/main.js
@@ -1,15 +1,3 @@
 $(document).ready(function() {
-    addElement();
+    console.log('== main.js loaded ==');
 });
-
-
-function addElement () {
-  // create a new div element
-  // and give it some content
-  var newDiv = document.getElementById("dataHold");
-  var newContent = document.createTextNode("Hi there and greetings!");
-  newDiv.appendChild(newContent); //add the text node to the newly created div.
-  // add the newly created element and its content into the DOM
-  var currentDiv = document.getElementById("div1");
-  document.body.insertBefore(newDiv, currentDiv);
-}
